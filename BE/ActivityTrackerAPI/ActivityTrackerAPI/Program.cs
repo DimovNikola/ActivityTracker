6547b505e7dacb118b42362cf4d4918285646029
DimovNikola <nikoladimov45@yahoo.com>
2023-05-08 22:13:36 +0200

    lets stick to web

--- BE/ActivityTrackerAPI/ActivityTrackerAPI/Program.cs
+++ BE/ActivityTrackerAPI/ActivityTrackerAPI/Program.cs
@@ -83,6 +83,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
 }
 
 app.UseHttpsRedirection();
